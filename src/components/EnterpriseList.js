4acd347527a58c09558be41c9c8ac50e04f3229e
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-09-27 17:06:57 -0300

    improved component EnterpriseList

--- src/components/EnterpriseList.js
+++ src/components/EnterpriseList.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-export default function EnterpriseList({ data = {} }) {
+export default function EnterpriseList(props) {
+  const printItems = () => {
+    if (props.items)
+      return props.items.map( (a) => { return <option key={a}>{a}</option> } );
+  };
+
   return (
       <div>
         <section>
           <div className="container">
-            <h4 className="enterpriseTitle">Empresa</h4>
+            <h4 className="enterpriseTitle">{props.title || 'Lista de Empresas'}</h4>
             <select>
-              <option>Socaltur</option>
-              <option>Wendling</option>
+              { printItems() }
             </select>
           </div>
         </section>
--- src/components/__tests__/EnterpriseList.spec.js
+++ src/components/__tests__/EnterpriseList.spec.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import EnterpriseList from '../EnterpriseList';
  
-it('renders enterprise list', () => {
+it('render EnterpriseList', () => {
   const wrapper = shallow(<EnterpriseList/>);
-  const textTitle = <h4 className="enterpriseTitle">Empresa</h4>;
-  const option = <option>Socaltur</option>;
+  const defaultTitle = <h4 className="enterpriseTitle">Lista de Empresas</h4>;
+  const emptySelect = <select></select>;
+  expect(wrapper.contains(defaultTitle)).toEqual(true);
+  expect(wrapper.contains(emptySelect)).toEqual(true);  
+});
+
+it('render EnterpriseList with props', () => {
+  const items = ['Socaltur','Wendling'];
+  const title = 'Empresa';
+  const wrapper = shallow(<EnterpriseList title={title} items={items}/>);
+  const textTitle = <h4 className="enterpriseTitle">{title}</h4>;
+  const option = <option>{items[0]}</option>;
   expect(wrapper.contains(textTitle)).toEqual(true);
   expect(wrapper.contains(option)).toEqual(true);
 });
